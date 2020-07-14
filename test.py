f9b18491af54d6d890db5f351a39ba38fbe97b19
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-14 10:29:40 -0700

    commit

--- test.py
+++ test.py
@@ -4,9 +4,10 @@
 import weiboo
 
 def test(key):
+	for url, card in weiboo.search(key).items():
+		print(url, weiboo.getHash(card))
 	with open('tmp.txt', 'w') as f:
-		for url, card in weiboo.search(key).items():
-			print(url, weiboo.getHash(card))
+		f.write(str(weiboo.search(key)))
 
 if __name__=='__main__':
 	test('女权')
