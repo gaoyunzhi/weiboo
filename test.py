3c7b76d713c1134f792f3f8c8b687f35976542d8
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-14 10:20:25 -0700

    commit

--- test.py
+++ test.py
@@ -4,8 +4,10 @@
 import weiboo
 
 def test(key):
-	print(weiboo.search(key))
+	with open('tmp.txt', 'w') as f:
+		for url, card in weiboo.search(key).items():
+			print(url, weiboo.getHash(card))
 
 if __name__=='__main__':
-	# test('女权')
+	test('女权')
 	test('7418114597')
--- weiboo/__init__.py
+++ weiboo/__init__.py
@@ -5,22 +5,50 @@ name = 'weiboo'
 import yaml
 import cached_url
 import urllib
+import re
+
+def isUser(key):
+	try:
+		int(key)
+		return True
+	except:
+		return False
 
 def getSearchUrl(key):
-	try:
+	if isUser(key):
 		user = int(key)
 		return 'https://m.weibo.cn/api/container/getIndex?type=uid&value=%d&containerid=107603%d' \
 			% (user, user)
-	except:
-		content_id = urllib.request.pathname2url('100103type=1&q=' + key)
-		return 'https://m.weibo.cn/api/container/getIndex?containerid=%s&page_type=searchall' % content_id
+	content_id = urllib.request.pathname2url('100103type=1&q=' + key)
+	return 'https://m.weibo.cn/api/container/getIndex?containerid=%s&page_type=searchall' % content_id
 
-def search(key, force_cache=False, cookie=None, sleep=0):
+def clearUrl(url):
+	return url.split('?')[0]
+
+def getTextHash(card):
+	result = []
+	for x in card.get('text', ''):
+		if re.search(u'[\u4e00-\u9fff]', x):
+			result.append(x)
+			if len(result) > 10:
+				break
+	return ''.join(result)
+
+def getHash(card):
+	card = card.get('mblog', card)
+	if card.get('retweeted_status'):
+		return getTextHash(card.get('retweeted_status'))
+	return getTextHash(card)
+
+def search(key, force_cache=False, sleep=0):
 	url = getSearchUrl(key)
-	# one of the case we don't need cookie, let me check which one
-	content = cached_url.get(url, 
-		headers = {'cookie': cookie}, sleep = sleep)
+	content = cached_url.get(url, force_cache=force_cache, 
+		sleep = sleep)
 	content = yaml.load(content, Loader=yaml.FullLoader)
+	result = {}
 	for card in content['data']['cards']:
 		if 'scheme' in card:
-			yielf card['scheme']
+			url = clearUrl(card['scheme'])
+			if '/status/' in url:
+				result[url] = card
+	return result
