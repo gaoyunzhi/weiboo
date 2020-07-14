40cb8b27800ac48d49d8c6b5367b55624dea736d
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-14 14:49:33 -0700

    commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="weiboo",
-    version="0.0.3",
+    version="0.0.4",
     author="Yunzhi Gao",
     author_email="gaoyunzhi@gmail.com",
     description="Plain txt DB",
